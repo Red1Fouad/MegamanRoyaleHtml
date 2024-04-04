3d9f7e03cc83e59f48cd364cf53be936c9a85255
Red1 <oradwane73@gmail.com>
2024-04-04 20:20:58 +0000

    force https

--- html5game/tph_gms2_socket.io.js
+++ html5game/tph_gms2_socket.io.js
@@ -18,7 +18,7 @@ class SocketIO {
         this.ip = ip;
         this.port = port;
 
-        this.socket = io.connect(`http://${this.ip}:${this.port}`);
+        this.socket = io.connect(`https://${this.ip}:${this.port}`);
 
         this.socket.on('connect' , () => {
             gml_Script_gmcallback_sio_on_connect();
